7ee66628145e7051b625739ef794a86929609445
Jared <jgnuunezz20@gmail.com>
2025-01-23 21:58:33 -0600

    Final de avance 23 de enero

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 let nombre = ""
-console.log(nombre)
-
 
 function agregarAmigo (){
     nombre = document.getElementById("amigo").value
@@ -12,9 +10,19 @@ function agregarAmigo (){
         amigos.push(nombre)
     }
     console.log(amigos)
+    agregarALaLista()
     limpiarDatos()
+    return nombre
 }
 
 function limpiarDatos () {
     document.querySelector("#amigo").value = ''
+}
+
+function agregarALaLista () {
+    console.log(nombre)
+    let unorderedList = document.querySelector('ul')
+    let lista = document.createElement('li')
+    lista.textContent = nombre 
+    unorderedList.appendChild(lista)
 }
