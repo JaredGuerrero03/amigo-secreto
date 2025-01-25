a22b88cf2548df3b4d4819bea36486ab6f11a590
Jared <jgnuunezz20@gmail.com>
2025-01-24 19:04:59 -0600

    Solución a una alerta de que el nombre ya estaba añadido sin haber nombres antes

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ function agregarAmigo (){
     nombre = document.getElementById("amigo").value
     if (nombre ===""){
         alert('Ingresa un nombre valido')
+    } else if (amigos.includes(nombre)) {
+        alert('El nombre ya fue registrado')
     } else {
         amigos.push(nombre)
     }
     console.log(amigos)
+    
     agregarALaLista()
     limpiarDatos()
     return nombre
 }
 
-function limpiarDatos () {
+
+function limpiarDatos () { 
     document.querySelector("#amigo").value = ''
 }
 
@@ -23,6 +27,7 @@ function agregarALaLista () {
     console.log(nombre)
     let unorderedList = document.querySelector('ul')
     let lista = document.createElement('li')
-    lista.textContent = nombre 
+    lista.textContent = nombre
     unorderedList.appendChild(lista)
+    
 }
